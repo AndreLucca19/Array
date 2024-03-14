72f297eb0ae864e798ba044c5e7170719bbcdeed
André Lucca <luccaandre78@gmail.com>
2024-03-14 17:14:08 -0300

    nomes e numeros com nomes adicionados

--- atividade.js
+++ atividade.js
@@ -1,1 +1,12 @@
-let nomes []
+let nomes = [];
+
+//Nomes adicionados
+nomes.push('Thiago');
+nomes.push('Felipe');
+nomes.push('André');
+nomes.push('João');
+
+console.log(nomes);
+
+let NomesMaisNumeros = nomes.length
+console.log(NomesMaisNumeros);
