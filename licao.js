40fee1f94cb9c76dec51ac281b768e94ab6b385c
Andre Lucca <luccaandre78@gmail.com>
2024-03-15 23:25:07 -0300

    pop

---
+++ licao.js
@@ -0,0 +1,15 @@
+let nomes = [];
+
+//Nomes adicionados
+nomes.push('Thiago');
+nomes.push('Felipe');
+nomes.push('André');
+nomes.push('João');
+
+console.log(nomes);
+
+
+//Tirando nomes (pop)
+let carros = ['Mercedes', 'Porsche', 'Lamborghini', 'Corsa'];
+let removerUltimoDaLista = carros.pop();
+console.log(carros);
