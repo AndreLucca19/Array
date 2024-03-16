d9c23c6b6870c8b335f6e105597f48fd7dbc4c7d
Andre Lucca <luccaandre78@gmail.com>
2024-03-16 14:46:14 -0300

    unshift

--- licao.js
+++ licao.js
@@ -1,15 +1,18 @@
-let nomes = [];
+let nomes = ['Thiago, Bruno, Felipe'];
 
 //Nomes adicionados
-nomes.push('Thiago');
-nomes.push('Felipe');
 nomes.push('André');
 nomes.push('João');
 
 console.log(nomes);
 
 
-//Tirando nomes (pop)
+//Tirando nomes (pop)'
 let carros = ['Mercedes', 'Porsche', 'Lamborghini', 'Corsa'];
 let removerUltimoDaLista = carros.pop();
 console.log(carros);
+
+//Adiciona um elemento ao inicio da Array
+let numeros = [1,2,3]
+numeros.unshift(0);
+console.log(numeros);
